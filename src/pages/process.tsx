8d07c53c49569d3bc8cc4d7b30d0b0ccb723cf2e
sam247 <sampettiford@googlemail.com>
2025-12-09 16:37:47 +0000

    Fix process ref callback type

--- src/pages/process.tsx
+++ src/pages/process.tsx
@@ -163,11 +163,13 @@ const Process = () => {
         <section className="pb-32">
           <div className="container mx-auto px-6 lg:px-12">
             {processSteps.map((step, index) => (
-              <div
-                key={step.number}
-                ref={(el) => (stepsRef.current[index] = el)}
-                className="py-20 lg:py-32 border-t border-border first:border-t-0"
-              >
+            <div
+              key={step.number}
+              ref={(el) => {
+                stepsRef.current[index] = el;
+              }}
+              className="py-20 lg:py-32 border-t border-border first:border-t-0"
+            >
                 <div className={`grid grid-cols-1 lg:grid-cols-2 gap-16 lg:gap-24 items-center ${
                   index % 2 === 1 ? 'lg:flex-row-reverse' : ''
                 }`}>
