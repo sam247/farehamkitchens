cf612698542ffcc571bc814e82da97989d8ae59c
sam247 <sampettiford@googlemail.com>
2026-03-03 18:00:00 +0000

    Remove CLAUDE.md file and add Microsoft Bing UET tracking scripts to _document.tsx

--- src/pages/_document.tsx
+++ src/pages/_document.tsx
@@ -32,6 +32,18 @@ export default function Document() {
           async
           defer
         />
+        {/* Microsoft Bing UET */}
+        <script
+          dangerouslySetInnerHTML={{
+            __html: `(function(w, d, t, u, o) {w[u] = w[u] || [], o.ts = (new Date).getTime();var n = d.createElement(t);n.src = "https://bat.bing.net/bat.js?ti=" + o.ti + ("uetq" != u ? "&q=" + u : ""),n.async = 1, n.onload = n.onreadystatechange = function() {var s = this.readyState;s && "loaded" !== s && "complete" !== s ||(o.q = w[u], w[u] = new UET(o), w[u].push("pageLoad"),n.onload = n.onreadystatechange = null)};var i = d.getElementsByTagName(t)[0];i.parentNode.insertBefore(n, i);})(window, document, "script", "uetq", {ti: "97221042",enableAutoSpaTracking: true});`,
+          }}
+        />
+        <script
+          dangerouslySetInnerHTML={{
+            __html: `window.uetq = window.uetq || [];window.uetq.push('consent', 'default', {'ad_storage': 'denied'});`,
+          }}
+        />
+        {/* When user accepts cookies, call: window.uetq.push('consent', 'update', {'ad_storage': 'granted'}); */}
       </Head>
       <body>
         <Main />
